381b5cccbbd38013b408b1fbe10ffa8fa037bd92
SuriyaaBk <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-10 14:33:40 +0530

    Time: 2 ms (80.00%) | Memory: 9.7 MB (90.00%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.c
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.c
@@ -1,17 +1,11 @@
 bool canConstruct(char* s, int k) {
     int* hash = (int*)calloc(26, sizeof(int));
-    int n = 0, even = 0, odd = 0;
+    int n = 0, odd = 0;
     for(n = 0; s[n]; n++) {
         hash[s[n] - 'a'] += 1;
     }
     for(int i = 0; i < 26; i++) {
-        if(hash[i] > 0) {
-            if(hash[i] % 2 == 0) even += hash[i] / 2;
-            else {
-                even += hash[i] / 2;
-                odd += 1;
-            }
-        }
+        odd += hash[i] & 1;
     }
     free(hash);
     if(n < k || odd > k) return false;
