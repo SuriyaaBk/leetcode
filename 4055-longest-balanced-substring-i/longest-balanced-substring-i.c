6625100c5214c32656d9da1230bf191cebfc878a
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-12 09:31:32 +0530

    Time: 39 ms (94.12%) | Memory: 9.6 MB (47.06%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.c
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.c
@@ -4,14 +4,14 @@ int longestBalanced(char* s) {
     for(int i = 0; s[i]; i++) {
         for(int k = 0; k < 26; k++) freq[k] = 0;
         int uniq = 0, maxC = 1, maxF = 0;
-        for(int j = i; s[j]; j++) {
+        for(int j = i; j >= 0; j--) {
             int F = freq[s[j] - 'a'] += 1;
             uniq += (F == 1);
             if(F > maxF) {
                 maxF = F;
                 maxC = 1;
             } else if(F == maxF) maxC += 1;
-            if(uniq == maxC) ans = fmax(ans, j - i + 1);
+            if(uniq == maxC) ans = fmax(ans, i - j + 1);
         }
     }
     free(freq);
