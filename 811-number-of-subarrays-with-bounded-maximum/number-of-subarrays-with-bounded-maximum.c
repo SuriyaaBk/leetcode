084f239f857e61334006b746b4ab942db21d400e
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-12 11:14:10 +0530

    Time: 2 ms (44.44%) | Memory: 13.5 MB (0.00%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.c
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.c
@@ -1,13 +1,14 @@
 int numSubarrayBoundedMax(int* nums, int numsSize, int left, int right) {
     int ans = 0, c1 = 0, c2 = 0;
     for(int i = 0; i < numsSize; i++) {
-        if(nums[i] >= left && nums[i] <= right) {
-            c1 += 1 + c2;
-            c2 = 0;
-        } else if(nums[i] > right) { 
+        if(nums[i] > right) { 
             c1 = 0;
             c2 = 0;
-        } else c2 += 1;
+        } else if(nums[i] < left) c2 += 1;
+        else {
+            c1 += 1 + c2;
+            c2 = 0;
+        }
         ans += c1;
     }
     return ans;
