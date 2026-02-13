760a2c15f1957f26a207c8566b95d2a8d6488ed0
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-14 00:12:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (46.69%) - LeetSync

---
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubstringPresent(string s) {
+        int n = s.size(), hash[26][26] = {};
+        for(int i = 0; i < n - 1; i++) {
+            int a = s[i] - 'a';
+            int b = s[i + 1] - 'a';
+            hash[a][b] = 1;
+        }
+        for(int i = n - 1; i > 0; i--) {
+            int a = s[i] - 'a';
+            int b = s[i - 1] - 'a';
+            if(hash[a][b] == 1) return true;
+        }
+        return false;
+    }
+};
