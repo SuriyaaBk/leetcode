c30be2be03d8f5232271140ec08b060b762b1659
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-13 11:11:11 +0530

    Time: 3 ms (100.00%) | Memory: 21.9 MB (14.29%) - LeetSync

--- 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.c
+++ 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.c
@@ -2,6 +2,7 @@ int minOperations(char* s) {
     int ans = 0;
     int* hash = (int*)malloc(26 * sizeof(int));
     for(int i = 0; s[i]; i++) {
+        if(s[i] == 'b') return 25;
         hash[s[i] - 'a'] = 1;
     }
     for(int i = 1; i < 26; i++) {
