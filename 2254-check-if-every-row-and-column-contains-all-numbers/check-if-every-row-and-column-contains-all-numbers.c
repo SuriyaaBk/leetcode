adb6dbf542aab0f97c48b93ae96a6d4ce9a77f32
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-13 16:08:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (38.10%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.c
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.c
@@ -1,13 +1,13 @@
 bool checkValid(int** matrix, int matrixSize, int* matrixColSize) {
     int n = matrixSize;
-    int* row = (int*)calloc(n + 1, sizeof(int));
-    int* col = (int*)calloc(n + 1, sizeof(int));
+    int* row = (int*)calloc(n, sizeof(int));
+    int* col = (int*)calloc(n, sizeof(int));
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            row[matrix[i][j]] += 1;
-            col[matrix[j][i]] += 1; 
+            row[matrix[i][j] - 1] += 1;
+            col[matrix[j][i] - 1] += 1; 
         }
-        for(int k = 1; k <= n; k++) {
+        for(int k = 0; k < n; k++) {
             if(row[k] != i + 1 || col[k] != i + 1) return false;
         }
     }
