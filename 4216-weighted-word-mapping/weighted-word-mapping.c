3753fd0730113cf2ab201147044585a154ad0b9f
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-14 22:50:39 +0530

    Time: 27 ms (4.37%) | Memory: 10.2 MB (74.12%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.c
@@ -0,0 +1,13 @@
+char* mapWordWeights(char** words, int wordsSize, int* weights, int weightsSize) {
+    char* ans = (char*)malloc((wordsSize + 1) * sizeof(char));
+    for(int i = 0; i < wordsSize; i++) {
+        int sum = 0;
+        for(int j = 0; words[i][j]; j++) {
+            sum += weights[words[i][j] - 'a'];
+        }
+        printf("%d ",sum);
+        ans[i] = 25 - sum % 26 + 'a';
+    }
+    ans[wordsSize] = '\0';
+    return ans;
+}
