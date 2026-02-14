697ce1bf9f7571bbb2eb579539447cb65eeb102f
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-14 11:53:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (41.83%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.c
+++ 404-sum-of-left-leaves/sum-of-left-leaves.c
@@ -6,16 +6,11 @@
  *     struct TreeNode *right;
  * };
  */
-int sum;
-void calculate(struct TreeNode* root) {
-    if(root == NULL) return;
-    if(root -> left != NULL && root -> left -> left == NULL && root -> left -> right == NULL) sum += root -> left -> val;
-        calculate(root -> left);
-        calculate(root -> right);
-
-}
 int sumOfLeftLeaves(struct TreeNode* root) {
-    sum = 0;
-    calculate(root);
+    int sum = 0;
+    if(root == NULL) return 0;
+    if(root -> left != NULL && root -> left -> left == NULL && root -> left -> right == NULL) sum += root -> left -> val;
+    sum += sumOfLeftLeaves(root -> left);
+    sum += sumOfLeftLeaves(root -> right);
     return sum;
 }
