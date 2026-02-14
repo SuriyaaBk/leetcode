105bd8ed294f0005754f323156b4e6d8f6abe599
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-14 11:21:48 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (15.00%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.c
@@ -0,0 +1,18 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* lexicalOrder(int n, int* returnSize) {
+    int* ans = (int*)malloc(n * sizeof(int));
+    int a = 1;    
+    for(int i = 0; i < n; i++) {
+        ans[i] = a;
+        if(a * 10 <= n) {
+            a *= 10;
+        } else {
+            while(a % 10 == 9 || a == n) a /= 10;
+            a++;
+        }
+    }
+    *returnSize = n;
+    return ans;
+}
