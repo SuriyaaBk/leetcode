201417eb52f47eea38da8224ea25701ccbd5149b
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-14 16:04:11 +0530

    Time: 3 ms (79.28%) | Memory: 8.7 MB (94.74%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.c
+++ 459-repeated-substring-pattern/repeated-substring-pattern.c
@@ -1,8 +1,16 @@
 bool repeatedSubstringPattern(char* s) {
     int n = strlen(s);
-    char t[2 * n + 1];
-    strcpy(t, s);
-    strcat(t, s);
-    t[2 * n - 1] = '\0';
-    return strstr(t + 1, s);
+    for(int i = 1; i <= n / 2; i++) {
+        if(n % i == 0) {
+            int flag = true;
+            for(int j = i; j < n; j++) {
+                if(s[j] != s[j - i]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true) return true;
+        }
+    }
+    return false;
 }
