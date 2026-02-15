4ad07d9b4bcd091f923a5f79feac38bb34515c88
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-15 16:06:36 +0530

    Time: 32 ms (83.86%) | Memory: 28.8 MB (48.03%) - LeetSync

--- 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.c
+++ 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.c
@@ -4,7 +4,7 @@ int firstUniqueFreq(int* nums, int numsSize) {
     int* hash = (int*)calloc(max + 1, sizeof(int));
     int* freq = (int*)calloc(numsSize + 1, sizeof(int));
     for(int i = 0; i < numsSize; i++) hash[nums[i]] += 1;
-    for(int i = 0; i <= max; i++) freq[hash[i]] += 1;
+    for(int i = 1; i <= max; i++) freq[hash[i]] += 1;
     for(int i = 0; i < numsSize; i++) {
         if(freq[hash[nums[i]]] == 1) {
             ans = nums[i];
