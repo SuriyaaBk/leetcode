22ba0be00ad17d8740fa1b7548c7e158590c4050
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-16 11:56:08 +0530

    Time: 27 ms (22.22%) | Memory: 11.2 MB (88.89%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.c
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.c
@@ -4,7 +4,7 @@ int maximumJumps(int* nums, int numsSize, int target) {
     for(int i = 1; i < numsSize; i++) {
         dp[i] = -1;
         for(int j = 0; j < i; j++) {
-            if(dp[j] != -1 && (abs(nums[i] - nums[j])) <= target) dp[i] = fmax(dp[i], dp[j] + 1);
+            if(abs(nums[i] - nums[j]) <= target && dp[j] != -1) dp[i] = fmax(dp[i], dp[j] + 1);
         }
     }
     return dp[numsSize - 1];
