23db90324b3385a7f55aa44001e4ac9900051eda
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-16 12:16:12 +0530

    Time: 20 ms (0.00%) | Memory: 57.8 MB (0.00%) - LeetSync

---
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.c
@@ -0,0 +1,21 @@
+int* findOriginalArray(int* changed, int changedSize, int* returnSize) {
+    int* hash = (int*)calloc(200001, sizeof(int));
+    int* ans = (int*)malloc(changedSize * sizeof(int));
+    int j = 0;
+    for(int i = 0; i < changedSize; i++) hash[changed[i]] += 1;
+    for(int i = 0; i <= 100000; i++) {
+        while(hash[i] > 0 && hash[2 * i] >= hash[i]) {
+            ans[j++] = i;
+            hash[i]--;
+            hash[2*i]--;
+        }
+        if(hash[i] != 0) {
+            free(hash);
+            *returnSize = 0;
+            return ans;
+        }
+    }    
+    free(hash);
+    *returnSize = j;
+    return ans;
+}
