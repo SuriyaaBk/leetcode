833c8436d72d1c64fd9573a12a331db6b6d661f8
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-16 09:45:43 +0530

    Time: 50 ms (42.95%) | Memory: 55.2 MB (87.46%) - LeetSync

--- 190-reverse-bits/reverse-bits.ts
+++ 190-reverse-bits/reverse-bits.ts
@@ -1,6 +1,6 @@
 function reverseBits(n: number): number {
-    var rev = 0;
-    for(var i = 0; i < 32; i++) {
+    let rev = 0;
+    for(let i = 0; i < 32; i++) {
         rev = (rev << 1) | ((n >> i) & 1);
     }
     return rev;
