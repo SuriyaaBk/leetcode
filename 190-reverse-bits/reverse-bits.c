c4abdb92c6cb6e250ea7fcbe462dcef88add5b4a
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-16 09:09:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (7.43%) - LeetSync

--- 190-reverse-bits/reverse-bits.c
+++ 190-reverse-bits/reverse-bits.c
@@ -1,8 +1,7 @@
 int reverseBits(int n) {
     int rev = 0;
     for(int i = 0; i < 32; i++) {
-        rev = (rev << 1) | (n & 1);
-        n = n >> 1;
+        rev = (rev << 1) | ((n >> i) & 1);
     }
     return rev;
 }
