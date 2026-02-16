e669fc278c22a0c274616d5fafb2adc5a9cc01b5
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-16 12:18:39 +0530

    Time: 17 ms (0.00%) | Memory: 57.7 MB (0.00%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.c
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.c
@@ -1,9 +1,13 @@
 int* findOriginalArray(int* changed, int changedSize, int* returnSize) {
     int* hash = (int*)calloc(200001, sizeof(int));
     int* ans = (int*)malloc(changedSize * sizeof(int));
-    int j = 0;
-    for(int i = 0; i < changedSize; i++) hash[changed[i]] += 1;
-    for(int i = 0; i <= 100000; i++) {
+    int min = changed[0], max = changed[0], j = 0;
+    for(int i = 0; i < changedSize; i++) {
+        min = fmin(min, changed[i]);
+        max = fmax(max, changed[i]);
+        hash[changed[i]] += 1;
+    }
+    for(int i = min; i <= max; i++) {
         while(hash[i] > 0 && hash[2 * i] >= hash[i]) {
             ans[j++] = i;
             hash[i] -= 1;
