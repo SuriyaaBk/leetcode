5c8effec16d08458659b91ffd68654047e052d49
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-16 13:51:15 +0530

    Time: 29 ms (37.50%) | Memory: 30.1 MB (18.75%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.c
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.c
@@ -7,14 +7,14 @@ char* smallestPalindrome(char* s) {
     int l = 0, r = n - 1;
     char* ans = (char*)malloc((n + 1) * sizeof(char));
     for(int i = 0; i < 26; i++) {
-        if(hash[i] & 1 == 1) {
-            ans[n / 2] = 'a' + i;
-            hash[i] -= 1;
-        }
-        while(hash[i] > 0) {
+        while(hash[i] > 1) {
             ans[l++] = 'a' + i;
             ans[r--] = 'a' + i;
             hash[i] -= 2;
+        }
+        if(hash[i] == 1) {
+            ans[n / 2] = 'a' + i;
+            hash[i] -= 1;
         }
     }
     ans[n] = '\0';
