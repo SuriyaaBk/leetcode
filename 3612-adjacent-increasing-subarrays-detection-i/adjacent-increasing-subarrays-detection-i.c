941430283be04f7f8425b730f10bcef764498716
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-17 09:47:00 +0530

    Time: 15 ms (6.00%) | Memory: 9 MB (80.00%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.c
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.c
@@ -1,14 +1,17 @@
 bool hasIncreasingSubarrays(int* nums, int numsSize, int k) {
-    for(int i = 0; i <= numsSize - 2 * k; i++) {
-        int flag = true;
-        for(int j = i; j < i + k - 1; j++) {
+    int n = numsSize - 2 * k;
+    for(int i = 0; i <= n; i++) {
+        bool flag = true;
+        int m = i + k - 1;
+        for(int j = i; j < m; j++) {
             if(nums[j] >= nums[j + 1]) {
                 flag = false;
                 break;
             }
         }
         if(flag == true) {
-            for(int j = i + k; j < i + 2 * k - 1; j++) {
+            m =i + 2 * k - 1;
+            for(int j = i + k; j < m; j++) {
                 if(nums[j] >= nums[j + 1]) {
                     flag = false;
                     break;
