6fe7760587d382e4a88a8aceb0f683570f5a402e
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-17 12:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (58.33%) - LeetSync

--- 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.c
+++ 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.c
@@ -2,9 +2,9 @@ int findIntegers(int n) {
     int ans = 0, a = 1, b = 2;
     n += 1;
     while(n > 0) {
-        if(n & 1 && n & 2) ans = 0;
+        if((n & 3) == 3) ans = 0; // ends with 11
         ans += a * (n & 1);
-        int c = b;
+        int c = b; // fibonacci series
         b = a + b;
         a = c; 
         n >>= 1;
