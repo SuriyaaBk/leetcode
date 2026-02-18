7e257e17f9536e9e445ea091a9d89cbce7fbaab6
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-18 12:00:50 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (8.00%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cs
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cs
@@ -1,6 +1,5 @@
 public class Solution {
     public bool HasAlternatingBits(int n) {
-        int x = n ^ (n >> 1);
-        return (x & (x + 1)) == 0;
+        return (n & (n >> 1)) == 0 && (n | (n >> 2)) == n;
     }
 }
