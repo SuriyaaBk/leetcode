601fb29bd6515b3be972b87d0849dc9200b94b58
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-18 21:51:14 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (65.25%) - LeetSync

--- 89-gray-code/gray-code.c
+++ 89-gray-code/gray-code.c
@@ -4,9 +4,7 @@ int gray(int n) {
 int* grayCode(int n, int* returnSize) {
     n = 1 << n; //squaring n;
     int* ans=(int*)malloc(n * sizeof(int));
-    for(int i = 0; i < n; i++) {
-        ans[i] = gray(i);
-    }
+    for(int i = 0; i < n; i++) ans[i] = gray(i);
     *returnSize = n;
     return ans;
 }
