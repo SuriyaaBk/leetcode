d7c10fe2d59b6117437ebf75442e15f2d8826439
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-18 10:01:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (65.59%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.py
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.py
@@ -1,4 +1,3 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        x = n ^ (n >> 1)
-        return (x & (x + 1)) == 0
+        return (n & (n >> 1)) == 0 and (n | (n >> 2)) == n
