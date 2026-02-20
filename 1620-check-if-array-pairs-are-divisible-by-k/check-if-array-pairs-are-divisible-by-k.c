e36e74e037fbbf3afd51f1bf1349f29e6ffacf8a
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-20 21:36:32 +0530

    Time: 7 ms (8.33%) | Memory: 16.6 MB (25.00%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.c
@@ -0,0 +1,13 @@
+bool canArrange(int* arr, int arrSize, int k) {
+    int* hash = (int*)calloc(k, sizeof(int));
+    for(int i = 0; i < arrSize; i++) {
+        int rem = arr[i] % k;
+        if(rem < 0) rem += k;
+        hash[rem] += 1;
+    }
+    if(hash[0] % 2 != 0) return false;
+    for(int i = 1; i <= k / 2; i++) {
+        if(hash[i] != hash[k - i]) return false;
+    }
+    return true;
+}
