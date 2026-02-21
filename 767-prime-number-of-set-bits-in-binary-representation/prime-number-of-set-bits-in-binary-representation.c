2b0e9fa8c7716975b7ef76914fae647ffa50cae4
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-21 08:56:06 +0530

    Time: 27 ms (26.58%) | Memory: 8.1 MB (7.66%) - LeetSync

---
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.c
@@ -0,0 +1,17 @@
+int isPrime(int n) {
+    if(n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13 || n == 17 || n == 19) return 1;
+    return 0;
+}
+int CountOnes(int n) {
+    int x = 0;
+    while(n > 0) {
+        if(n & 1) x += 1;
+        n >>= 1;
+    }
+    return isPrime(x);
+}
+int countPrimeSetBits(int left, int right) {
+    int ans = 0;
+    for(int i = left; i <= right; i++) ans += CountOnes(i);
+    return ans;
+}
