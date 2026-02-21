2ecb06a49df89acc4ccc78eb6e796291d036fb05
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-21 11:31:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (59.38%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.c
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.c
@@ -1,11 +1,11 @@
 int getWinner(int* arr, int arrSize, int k) {
-    int ans = arr[0], n = 0;
+    int ans = arr[0], c = 0;
     for(int i = 1; i < arrSize; i++) {
         if(arr[i] > ans) {
             ans = arr[i];
-            n = 0;
-        }
-        if(++n == k) return ans;
+            c = 1;
+        } else c++;
+        if(c == k) return ans;
     }
     return ans;
 }
