1fa4a1b0dadc9aafa338ee367647a282652fe3c3
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-22 10:56:20 +0530

    Time: 42 ms (21.41%) | Memory: 33.8 MB (36.70%) - LeetSync

---
+++ 4044-maximum-bitwise-xor-after-rearrangement/maximum-bitwise-xor-after-rearrangement.c
@@ -0,0 +1,27 @@
+char* maximumXor(char* s, char* t) {
+    int n, ones = 0, zeros = 0;
+    for(n = 0; t[n]; n++) {
+        if(t[n] == '1') ones++;
+        else zeros++;
+    }
+    char* ans = (char*)malloc((n + 1) * sizeof(char));
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '0') {
+            if(ones > 0) {
+                ans[i] = '1';
+                ones--;
+            } else {
+                ans[i] = '0';
+            }
+        } else {
+            if(zeros > 0) {
+                ans[i] = '1';
+                zeros--;
+            } else {
+                ans[i] = '0';
+            }
+        }
+    }
+    ans[n] = '\0';
+    return ans;
+}
