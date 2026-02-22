d5ff22af9f345dc1346e9539619e3818af6281c5
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-22 10:33:11 +0530

    Time: 4 ms (7.61%) | Memory: 10.2 MB (30.72%) - LeetSync

--- 4224-find-the-score-difference-in-a-game/find-the-score-difference-in-a-game.c
+++ 4224-find-the-score-difference-in-a-game/find-the-score-difference-in-a-game.c
@@ -1,8 +1,8 @@
 int scoreDifference(int* nums, int numsSize) {
     int x = 0, y = 0, swap = 1;
     for(int i = 0; i < numsSize; i++) {
-        if(nums[i] % 2 == 1) swap ^= 1;
-        if(i % 6 == 5) swap ^= 1;
+        if(nums[i] & 1) swap ^= 1;
+        if((i + 1) % 6 == 0) swap ^= 1;
         if(swap) x += nums[i];
         else y += nums[i]; 
     }
