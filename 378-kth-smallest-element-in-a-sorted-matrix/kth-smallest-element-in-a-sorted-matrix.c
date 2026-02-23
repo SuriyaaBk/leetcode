d7a15685972381aa09214f49a361982ae7702371
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-23 22:40:41 +0530

    Time: 7 ms (8.25%) | Memory: 12.2 MB (5.15%) - LeetSync

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.c
@@ -0,0 +1,14 @@
+int compare(const void* a, const void* b) {
+    return *(int*)a > *(int*)b;
+}
+int kthSmallest(int** matrix, int matrixSize, int* matrixColSize, int k) {
+    int t = 0, n = matrixSize;
+    int* nums = (int*)malloc((n * n) * sizeof(int));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            nums[t++] = matrix[i][j];
+        }
+    }
+    qsort(nums, t, sizeof(int), compare);
+    return nums[k - 1];
+}
