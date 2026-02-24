e1643e71dd88506cb0972e075c5977148420d402
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-24 23:43:04 +0530

    Time: 38 ms (7.14%) | Memory: 16 MB (82.14%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.c
@@ -0,0 +1,12 @@
+int minimumSize(int* nums, int numsSize, int maxOperations) {
+    int l = 1, r = 1000000000;
+    while(l < r) {
+        int m = (l + r) / 2, c = 0;
+        for(int i = 0; i < numsSize; i++) {
+            c += (nums[i] - 1) / m;
+        }
+        if(c > maxOperations) l = m + 1;
+        else r = m;
+    }
+    return r;
+}
