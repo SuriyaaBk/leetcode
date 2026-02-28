c1359c28d713eb549c2c2c16026775f6e4cf783e
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-02-28 22:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (4.49%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool checkTree(struct TreeNode* root) {
+    int sum1 = root -> val, sum2 = 0;
+    struct TreeNode* root2 = root;
+    while(root -> left != NULL) {
+        root = root -> left;
+        sum2 += root -> val;
+    }
+    while(root2 -> right != NULL) {
+        root2 = root2 -> right;
+        sum2 += root2 -> val;
+    }
+    return sum1 == sum2;
+}
