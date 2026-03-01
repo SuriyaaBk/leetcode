b19cad501d4a6ec74135a46891ad3a675825a62a
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-01 09:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (9.50%) - LeetSync

--- 4229-trim-trailing-vowels/trim-trailing-vowels.c
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.c
@@ -1,8 +1,6 @@
 char* trimTrailingVowels(char* s) {
-    int i;
-    char* ans = (char*)malloc(101 * sizeof(char));
-    for(i = 0; s[i]; i++) ans[i] = s[i];
-    while(i > 0 && (ans[i - 1] == 'a' || ans[i - 1] == 'e' || ans[i - 1] == 'i' || ans[i - 1] == 'o' || ans[i - 1] == 'u')) i--;
-    ans[i] = '\0';
-    return ans;
+    int i = strlen(s) - 1;
+    while(i >= 0 && (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')) i--;
+    s[i + 1] = '\0';
+    return s;
 }
