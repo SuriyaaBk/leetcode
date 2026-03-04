208ae2b2686db347eb3c752f8e068f8c6d8e7482
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-04 09:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (8.33%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.c
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.c
@@ -3,22 +3,14 @@ int numSpecial(int** mat, int matSize, int* matColSize) {
     int* row = (int*)calloc(matSize, sizeof(int));
     int* col = (int*)calloc(matColSize[0], sizeof(int));
     for(int i = 0; i < matSize; i++) {
-        int sum = 0;
         for(int j = 0; j < matColSize[0]; j++) {
-            sum += mat[i][j];
+            row[i] += mat[i][j];
+            col[j] += mat[i][j];
         }
-        if(sum == 1) row[i] = 1;
-    }  
-    for(int j = 0; j < matColSize[0]; j++) {
-        int sum = 0;
-        for(int i = 0; i < matSize; i++) {
-            sum += mat[i][j];
-        }
-        if(sum == 1) col[j] = 1;
-    }  
+    }   
     for(int i = 0; i < matSize; i++) {
         for(int j = 0; j < matColSize[0]; j++) {
-            if(mat[i][j] && row[i] && col[j]) ans++;
+            if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1) ans++;
         }
     }
     free(row);
