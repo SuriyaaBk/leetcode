fee0a484166210bf52aa7d323dade1e6806265ab
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-06 10:03:37 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (54.76%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -1,6 +1,3 @@
 var checkOnesSegment = function(s) {
-    for(let i = 1; i < s.length; i++) {
-        if(s[i] == '1' && s[i - 1] == '0') return false;
-    }
-    return true;
+    return !s.includes("01");
 };
