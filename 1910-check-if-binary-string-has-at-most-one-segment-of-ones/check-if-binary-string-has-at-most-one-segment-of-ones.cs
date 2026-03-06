bb89ce3a361c04cfcc6865f417f4d5bb951193d5
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-06 10:03:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (24.14%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cs
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cs
@@ -1,8 +1,5 @@
 public class Solution {
     public bool CheckOnesSegment(string s) {
-        for(int i = 1; i < s.Length; i++) {
-            if(s[i] == '1' && s[i - 1] == '0') return false;
-        }
-        return true;
+        return !s.Contains("01");
     }
 }
