76e861e57dbb9d531b730dee04b0b9055b428cff
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-07 19:25:00 +0530

    Time: 11 ms (87.50%) | Memory: 10.2 MB (75.00%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.c
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.c
@@ -1,10 +1,10 @@
 int minFlips(char* s) {
     int a[2] = {0, 0};
-    int ans = INT_MAX, x = 0, y = 0, n = 0;
-    for(n = 0; s[n]; n++, x ^= 1) {
-        a[(s[n] - '0') ^ x]++;
+    int ans = 100000, x = 0, y = 0;
+    for(int i = 0; s[i]; i++, x ^= 1) {
+        a[(s[i] - '0') ^ x]++;
     }
-    for(int i = 0; i < n; i++, x ^= 1, y ^= 1) {
+    for(int i = 0; s[i]; i++, x ^= 1, y ^= 1) {
         a[(s[i] - '0') ^ y]--;
         a[(s[i] - '0') ^ x]++;
         ans = fmin(ans, fmin(a[0], a[1]));
