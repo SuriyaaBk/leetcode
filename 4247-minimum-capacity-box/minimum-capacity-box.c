3ddccd0fa8e850a4a463f05d9f9ffce8e1d54fa4
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-08 12:08:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (24.81%) - LeetSync

--- 4247-minimum-capacity-box/minimum-capacity-box.c
+++ 4247-minimum-capacity-box/minimum-capacity-box.c
@@ -1,12 +1,9 @@
 int minimumIndex(int* capacity, int capacitySize, int itemSize) {
-    int min_diff = INT_MAX, ans = -1;
+    int min_cap = INT_MAX, ans = -1;
     for(int i = 0; i < capacitySize; i++) {
-        if(capacity[i] >= itemSize) {
-            int diff = capacity[i] - itemSize;
-            if(diff < min_diff) {
-                min_diff = diff;
-                ans = i;
-            }
+        if(capacity[i] >= itemSize && capacity[i] < min_cap) {
+            min_cap = capacity[i];
+            ans = i;
         }
     }
     return ans;
