37e5e7440dd72c3dca795201c770a4654101bc0e
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-08 12:18:52 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (78.11%) - LeetSync

--- 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.c
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.c
@@ -2,13 +2,8 @@ int smallestBalancedIndex(int* nums, int numsSize) {
     long long sum = 0, pro = 1;
     int l = 0, r = numsSize - 1;
     while(l < r) {
-        if(sum < pro) {
-            sum += nums[l];
-            l++;
-        } else {
-            pro *= nums[r];
-            r--;
-        }
+        if(sum < pro) sum += nums[l++];
+        else pro *= nums[r--];
     }
     if(sum == pro) return l;
     return -1;
