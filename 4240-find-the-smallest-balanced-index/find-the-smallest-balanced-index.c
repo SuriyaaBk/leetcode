e0dd6d418da0194206e98a42bd75589d216db740
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-08 12:18:04 +0530

    Time: 2 ms (93.49%) | Memory: 23.8 MB (78.11%) - LeetSync

--- 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.c
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.c
@@ -1,11 +1,15 @@
 int smallestBalancedIndex(int* nums, int numsSize) {
     long long sum = 0, pro = 1;
-    for(int i = 0; i < numsSize; i++) sum += nums[i];
-    for(int i = numsSize - 1; i >= 0; i--) {
-        sum -= nums[i];
-        if(sum == pro) return i;
-        if(pro  >= LONG_MAX / nums[i]) return -1;
-        pro *= nums[i];
+    int l = 0, r = numsSize - 1;
+    while(l < r) {
+        if(sum < pro) {
+            sum += nums[l];
+            l++;
+        } else {
+            pro *= nums[r];
+            r--;
+        }
     }
+    if(sum == pro) return l;
     return -1;
 }
