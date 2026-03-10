eab1937300c05f959a4db37037fde39fa0c4e730
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-10 20:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (10.71%) - LeetSync

--- 3330-modify-the-matrix/modify-the-matrix.c
+++ 3330-modify-the-matrix/modify-the-matrix.c
@@ -1,21 +1,17 @@
 int** modifiedMatrix(int** matrix, int matrixSize, int* matrixColSize, int* returnSize, int** returnColumnSizes) {
     int n = matrixSize, m = matrixColSize[0];
-    int* col_max = (int*)malloc(m * sizeof(int));
     *returnColumnSizes = (int*)malloc(n * sizeof(int));
     for(int j = 0; j < m; j++) {
         int max = matrix[0][j];
         for(int i = 1; i < n; i++) {
             if(matrix[i][j] > max) max = matrix[i][j];
         }
-        col_max[j] = max;
-    }    
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(matrix[i][j] == -1) matrix[i][j] = col_max[j];
+        for(int i = 0; i < n; i++) {
+            if(matrix[i][j] == -1) matrix[i][j] = max;
+            (*returnColumnSizes)[i] = m;
         }
-        (*returnColumnSizes)[i] = m;
-    }
-    free(col_max);
+    }    
+
     *returnSize = n;
     return matrix;
 }
