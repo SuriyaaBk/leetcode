17d12adab5326cbabe92781d2614f230840d268f
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-14 15:46:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (14.29%) - LeetSync

---
+++ 812-rotate-string/rotate-string.c
@@ -0,0 +1,9 @@
+bool rotateString(char* s, char* goal) {
+    int len1 = strlen(s);
+    int len2 = strlen(goal);
+    if(len1 != len2) return false;
+    char temp[2 * len1 + 1];
+    strcpy(temp, s);
+    strcat(temp, s);
+    return strstr(temp, goal) != NULL;
+}
