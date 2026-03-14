26d0f85b544b40955b7538ed1e6a7b856619305d
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-14 22:12:54 +0530

    Time: 110 ms (76.00%) | Memory: 124.4 MB (12.00%) - LeetSync

--- 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.c
+++ 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.c
@@ -1,17 +1,13 @@
 int minCost(int* nums1, int nums1Size, int* nums2, int nums2Size) {
     int ans = 0;
-    int* hash1 = (int*)malloc(80001 * sizeof(int));
-    int* hash2 = (int*)malloc(80001 * sizeof(int));
-    for(int i = 0; i <= 80000; i++) hash1[i] = hash2[i] = 0;
+    int* hash = (int*)calloc(80001, sizeof(int));
     for(int i = 0; i < nums1Size; i++) {
-        hash1[nums1[i]]++;
-        hash2[nums2[i]]++;
+        hash[nums1[i]]++;
+        hash[nums2[i]]--;
     }
     for(int i = 0; i <= 80000; i++) {
-        if(hash1[i] > 0 || hash2[i] > 0) {
-            if(((hash1[i] + hash2[i]) & 1) == 1) return -1;
-            ans += ((abs(hash1[i] - hash2[i])) / 2);
-        }
+        if(hash[i] & 1) return -1;
+        if(hash[i] > 0) ans += hash[i] / 2;
     }
-    return ans / 2;
+    return ans;
 }
