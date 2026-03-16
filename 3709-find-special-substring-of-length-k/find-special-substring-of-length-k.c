3e0749f4cabfc3b17f4c2696577969415510a9b3
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-16 21:26:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.00%) - LeetSync

--- 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.c
+++ 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.c
@@ -1,10 +1,9 @@
 bool hasSpecialSubstring(char* s, int k) {
     int c = 1;
-    for(int i = 1; s[i]; i++) {
-        if(s[i] == s[i - 1]) c++;
-        else {
+    for(int i = 1; s[i]; i++, c++) {
+        if(s[i] != s[i - 1]) {
             if(c == k) return true;
-            c = 1;
+            c = 0;
         }
     }
     if(c == k) return true;
