d89769dc35ea3531add8298d05de4e9ef7a550a3
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-03-17 18:25:55 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (20.00%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.c
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.c
@@ -1,8 +1,8 @@
 long long maxTotalValue(int* nums, int numsSize, int k) {
     int max = nums[0], min = nums[0];
     for(int i = 1; i < numsSize; i++) {
-        max = fmax(max, nums[i]);
-        min = fmin(min, nums[i]);
+        if(nums[i] > max) max = nums[i];
+        else if(nums[i] < min) min = nums[i];
     }
     return ((long long) max - min) * k;
 }
