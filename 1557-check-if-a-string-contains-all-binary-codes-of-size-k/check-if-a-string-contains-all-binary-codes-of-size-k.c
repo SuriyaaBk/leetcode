7804e8684333aa6c2923018771138f92fad6bf29
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-06 11:53:05 +0530

    Time: 16 ms (27.42%) | Memory: 39.6 MB (5.63%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.c
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.c
@@ -3,8 +3,7 @@ bool hasAllCodes(char* s, int k) {
     int* hash = (int*)calloc(uniq_nums, sizeof(int));
     for(int i = 0; s[i]; i++) {
         num = ((num << 1) & mask) | (s[i] & 1);
-        if(i < k - 1) continue;
-        if(hash[num] == 0) {
+        if(i + 1 >= k && hash[num] == 0) {
             hash[num] = 1;
             uniq_nums--;
             if(uniq_nums == 0) return true;
