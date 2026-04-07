c8da7e41965301d94250801c2119aa2c94bff167
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-07 19:04:57 +0530

    Time: 95 ms (16.67%) | Memory: 197.9 MB (5.56%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.c
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.c
@@ -9,14 +9,14 @@ int** findWinners(int** matches, int matchesSize, int* matchesColSize, int* retu
     int** ans = (int**)calloc(2, sizeof(int*));
     for(int i = 0; i < 100001; i++) wins[i] = loss[i] = 0;
     for(int i = 0; i < matchesSize; i++) {
-        wins[matches[i][0]]++;
+        wins[matches[i][0]] = 1;
         loss[matches[i][1]]++;
     }
     ans[0] = (int*)malloc(matchesSize * sizeof(int));
     ans[1] = (int*)malloc(matchesSize * sizeof(int));
     int w = 0, l = 0;
     for(int i = 0; i < 100001; i++) {
-        if(wins[i] != 0 && loss[i] == 0) ans[0][w++] = i;
+        if(wins[i] == 1 && loss[i] == 0) ans[0][w++] = i;
         else if(loss[i] == 1) ans[1][l++] = i;
     }
     free(wins);
