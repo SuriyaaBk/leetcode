a0995e0dabde1c03e40517b9aef677431ae78800
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-07 18:02:42 +0530

    Time: 3 ms (45.39%) | Memory: 9.2 MB (23.21%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.c
+++ 657-robot-return-to-origin/robot-return-to-origin.c
@@ -1,5 +1,5 @@
 bool judgeCircle(char* moves) {
-    int d1 = 0,d2 = 0;
+    int d1 = 0, d2 = 0;
     for(int i = 0; moves[i] != '\0'; i++) {
         if(moves[i] == 'U') d1++;
         else if(moves[i] == 'D') d1--;
