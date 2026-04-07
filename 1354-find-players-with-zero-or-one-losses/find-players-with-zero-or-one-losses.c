7fe9f16bef4e10ddf270519de21f96d0a89c374c
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-07 19:03:01 +0530

    Time: 82 ms (16.67%) | Memory: 197.7 MB (5.56%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.c
@@ -0,0 +1,32 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** findWinners(int** matches, int matchesSize, int* matchesColSize, int* returnSize, int** returnColumnSizes) {
+    int* wins = (int*)malloc(100001 * sizeof(int));
+    int* loss = (int*)malloc(100001 * sizeof(int));
+    int** ans = (int**)calloc(2, sizeof(int*));
+    for(int i = 0; i < 100001; i++) {
+        wins[i] = 0;
+        loss[i] = 0;
+    }
+    for(int i = 0; i < matchesSize; i++) {
+        wins[matches[i][0]]++;
+        loss[matches[i][1]]++;
+    }
+    ans[0] = (int*)malloc(matchesSize * sizeof(int));
+    ans[1] = (int*)malloc(matchesSize * sizeof(int));
+    int w = 0, l = 0;
+    for(int i = 0; i < 100001; i++) {
+        if(wins[i] != 0 && loss[i] == 0) ans[0][w++] = i;
+        else if(loss[i] == 1) ans[1][l++] = i;
+    }
+    free(wins);
+    free(loss);
+    *returnColumnSizes = (int*)malloc(2 * sizeof(int));
+    (*returnColumnSizes)[0] = w;
+    (*returnColumnSizes)[1] = l;
+    *returnSize = 2;
+    return ans;
+}
