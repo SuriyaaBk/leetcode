e1b4b6d711b3a8edb6efe6bf34b87d2979f8ec6a
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-08 11:00:19 +0530

    Time: 63 ms (83.33%) | Memory: 25.7 MB (0.00%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.c
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.c
@@ -1,14 +1,10 @@
 int xorAfterQueries(int* nums, int numsSize, int** queries, int queriesSize, int* queriesColSize) {
     int ans = 0;
     for(int i = 0; i < queriesSize; i++) {
-        int l = queries[i][0];
-        int r = queries[i][1];
-        int k = queries[i][2];
-        int v = queries[i][3];
-        int idx = l;
-        while(idx <= r) {
-            nums[idx] = ((long)nums[idx] * v) % (1000000007);
-            idx += k;
+        int l = queries[i][0], r = queries[i][1], k = queries[i][2], v = queries[i][3];
+        while(l <= r) {
+            nums[l] = ((long)nums[l] * v) % (1000000007);
+            l += k;
         }
     }
     for(int i = 0; i < numsSize; i++) ans ^= nums[i];
