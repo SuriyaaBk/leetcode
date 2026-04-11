a5ff82fc3b75de5be07f91f4d7ec4b1bcb8385ad
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-11 23:28:14 +0530

    Time: 1418 ms (5.26%) | Memory: 257.5 MB (5.26%) - LeetSync

--- 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.c
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.c
@@ -1,17 +1,14 @@
+int isPrime(int n) {
+    for(int i = 3; i*i <= n; i+=2) {
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
 int minOperations(int* nums, int numsSize) {
     int ans = 0;
     int* prime = (int*)calloc(100004, sizeof(int));
     prime[2] = 1;
-    for(int i = 3; i <= 100003; i+=2) {
-        int c = 1;
-        for(int j = 3; j*j <= i; j+=2) {
-            if(i % j == 0) {
-                c = 0;
-                break;
-            }
-        }
-        prime[i] = c;
-    }
+    for(int i = 3; i <= 100003; i+= 2) prime[i] = isPrime(i);
     for(int i = 0; i < numsSize; i+=2) {
         int n = nums[i], m = n;
         while(prime[m] == 0) m++;
