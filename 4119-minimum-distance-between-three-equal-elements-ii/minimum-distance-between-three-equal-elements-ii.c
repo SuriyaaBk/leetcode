e11e27214b40c0c14973f054343ac7af93de1442
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-11 22:51:59 +0530

    Time: 348 ms (100.00%) | Memory: 22.7 MB (100.00%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.c
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.c
@@ -1,9 +1,6 @@
 int minimumDistance(int* nums, int numsSize) {
-    int ans = INT_MAX;
-    int hash[100001][3];
-    for(int i = 0; i <= 100000; i++) {
-        hash[i][0] = hash[i][1] = hash[i][2] = -1;
-    }
+    int ans = INT_MAX, hash[100001][3];
+    for(int i = 0; i <= 100000; i++) hash[i][0] = hash[i][1] = hash[i][2] = -1;
     for(int i = 0; i < numsSize; i++) {
         int n = nums[i];
         hash[n][2] = hash[n][1];
