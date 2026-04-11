4f52e460ea0435e2c4fe5f88db73a8dbdcff0608
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-11 23:42:24 +0530

    Time: 283 ms (55.79%) | Memory: 257.5 MB (5.26%) - LeetSync

--- 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.c
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.c
@@ -1,22 +1,22 @@
-int isPrime(int n) {
-    if(n == 1) return 0;
-    if(n == 2) return 1;
-    if(n % 2 == 0) return 0;
-    for(int i = 3; i*i <= n; i+=2) {
-        if(n % i == 0) return 0;
-    }
-    return 1;
-}
 int minOperations(int* nums, int numsSize) {
     int ans = 0;
-    for(int i = 0; i < numsSize; i++) {
-        int n = nums[i], m = n;
-        if(i & 1) {
-            while(isPrime(m)) m++; 
-        } else {
-            while(!(isPrime(m))) m++;
+    int* prime = (int*)calloc(100004, sizeof(int));
+    prime[1] = 1;
+    for(long i = 2; i <= 100003; i++) {
+        if(prime[i] == 0) {
+            for(long j = i*i; j <= 100003; j+=i) prime[j] = 1;
         }
+    }
+    for(int i = 0; i < numsSize; i+=2) {
+        int n = nums[i], m = n;
+        while(prime[m] == 1) m++;
         ans += m - n;
     }
+    for(int i = 1; i < numsSize; i+=2) {
+        int n = nums[i], m = n;
+        while(prime[m] == 0) m++;
+        ans += m - n;
+    }
+    free(prime);
     return ans;
 }
