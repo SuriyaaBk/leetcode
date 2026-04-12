9f22989c3f32250473e2c1fcd3e966fa6f942802
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-12 23:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (30.00%) - LeetSync

--- 1346-maximize-the-topmost-element-after-k-moves/maximize-the-topmost-element-after-k-moves.c
+++ 1346-maximize-the-topmost-element-after-k-moves/maximize-the-topmost-element-after-k-moves.c
@@ -1,6 +1,6 @@
 int maximumTop(int* nums, int numsSize, int k) {
     if(numsSize == 1 && k & 1) return -1;
-    int max = 0;
+    int max = -1;
     for(int i = 0; i < numsSize && i < k - 1; i++) max = fmax(max, nums[i]);
     if(k < numsSize) max = fmax(max, nums[k]);
     return max;
