dcaae8edc11ef732a339f69161c61e5be9fd4162
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-13 11:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (44.74%) - LeetSync

--- 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.c
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.c
@@ -1,7 +1,7 @@
 int getMinDistance(int* nums, int numsSize, int target, int start) {
     int ans = numsSize;
     for(int i = 0; i < numsSize; i++) {
-        if(nums[i] == target) ans = fmin(ans, abs(start - i));
+        if(nums[i] == target) ans = fmin(ans, abs(i - start));
     }
     return ans;
 }
