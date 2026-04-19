a78907feeed659cd398e41253b28f720751eb1f7
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-19 11:23:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (16.67%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.c
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.c
@@ -1,11 +1,13 @@
 int firstStableIndex(int* nums, int numsSize, int k) {
-    int max = nums[0], min;
+    int* min = (int*)malloc(numsSize * sizeof(int));
+    min[numsSize - 1] = nums[numsSize - 1];
+    for(int i = numsSize - 2; i >= 0; i--) {
+        min[i] = fmin(min[i+1], nums[i]);
+    }
+    int max = nums[0];
     for(int i = 0; i < numsSize; i++) {
-        max = fmax(max, nums[i]), min = nums[i];
-        for(int j = i; j < numsSize; j++) {
-            min = fmin(min, nums[j]);
-        }
-        if(max - min <= k) return i;
+        max = fmax(max, nums[i]);
+        if(max - min[i] <= k) return i;
     }
     return -1;
 }
