84f81af3b0cabf109a906a3c8528a117e2a749eb
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-23 21:59:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (24.39%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.c
@@ -0,0 +1,16 @@
+char* capitalizeTitle(char* title) {
+    int i = 0, j = 0;
+    while(title[j]) {
+        if(title[j] >= 'A' && title[j] <= 'Z') title[j] += 32;
+        else if(title[j] == ' ') {
+            if(j - i > 2) {
+                if(title[i] >= 'a') title[i] -= 32;
+            } else if(title[i] <= 'Z') title[i] += 32;
+            i = j + 1;
+        } 
+        j++;
+    }
+    if(j - i > 2 && title[i] >= 'a') title[i] -= 32;
+    else if(title[i] <= 'Z') title[i] += 32;
+    return title; 
+}
