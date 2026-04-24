15a605d3770d76cce82c9391e583b0325229d39c
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-24 09:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (85.71%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.c
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.c
@@ -1,9 +1,9 @@
 int furthestDistanceFromOrigin(char* moves) {
-    int left = 0, right = 0, dash = 0;
+    int points = 0, dash = 0;
     for(int i = 0; moves[i]; i++) {
         if(moves[i] == '_') dash++;
-        else if(moves[i] == 'L') left++;
-        else right++;
+        else if(moves[i] == 'L') points--;
+        else points++;
     }
-    return dash + abs(left - right);
+    return dash + abs(points);
 }
