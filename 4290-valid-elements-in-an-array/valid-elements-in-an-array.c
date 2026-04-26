b80e4bce701e671edb8bcba57f8e6bbb1a634b53
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-26 22:40:49 +0530

    Time: 4 ms (8.82%) | Memory: 12.2 MB (38.24%) - LeetSync

---
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findValidElements(int* nums, int numsSize, int* returnSize) {
+    if(numsSize == 1) {
+        *returnSize = 1;
+        return nums;
+    }
+    int* right = (int*)malloc(numsSize * sizeof(int));
+    int* ans = (int*)malloc(numsSize * sizeof(int));
+    int left = nums[0], ind = 0, n = numsSize - 1;
+    ans[0] = nums[0], right[n] = nums[n];
+    for(int i = n - 1; i >= 0; i--) right[i] = fmax(nums[i], right[i+1]);
+    for(int i = 1; i < n; i++) {
+        if(nums[i] > left || nums[i] > right[i+1]) ans[++ind] = nums[i];
+        left = fmax(left, nums[i]);
+    }
+    ans[++ind] = nums[n];
+    free(right);
+    *returnSize = ind + 1;
+    return ans;
+}
