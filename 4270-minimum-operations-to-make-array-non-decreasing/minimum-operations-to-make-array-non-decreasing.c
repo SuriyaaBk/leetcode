17f16e7acc0dbf784a236ec77367bba32dbda3b5
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-26 22:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (84.09%) - LeetSync

--- 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.c
+++ 4270-minimum-operations-to-make-array-non-decreasing/minimum-operations-to-make-array-non-decreasing.c
@@ -1,5 +1,7 @@
 long long minOperations(int* nums, int numsSize) {
     long long ans = 0;
-    for(int i = 1; i < numsSize; i++) ans += fmax(0, nums[i-1] - nums[i]);
+    for(int i = 1; i < numsSize; i++) {
+        if(nums[i-1] > nums[i]) ans += nums[i-1] - nums[i];
+    }
     return ans;
 }
