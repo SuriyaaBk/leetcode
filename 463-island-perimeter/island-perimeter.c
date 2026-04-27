f25f461449e386f6a67ebb61b3e37f24de69724f
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-27 13:13:14 +0530

    Time: 9 ms (10.88%) | Memory: 15.2 MB (44.56%) - LeetSync

--- 463-island-perimeter/island-perimeter.c
+++ 463-island-perimeter/island-perimeter.c
@@ -3,10 +3,9 @@ int islandPerimeter(int** grid, int gridSize, int* gridColSize) {
     for(int i = 0; i < r; i++) {
         for(int j = 0; j < c; j++) {
             if(grid[i][j] == 1) {
-                if(i == 0 || grid[i-1][j] == 0) ans += 1;
-                if(j == 0 || grid[i][j-1] == 0) ans += 1;
-                if(i+1 == r || grid[i+1][j] == 0) ans += 1;
-                if(j+1 == c || grid[i][j+1] == 0) ans += 1;
+                ans += 4;
+                if(i > 0 && grid[i-1][j] == 1) ans -= 2;
+                if(j > 0 && grid[i][j-1] == 1) ans -= 2;
             }
         }
     }
