977805479c61cbefa847af64198dff2853e7dba6
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-28 23:03:55 +0530

    Time: 73 ms (50.00%) | Memory: 33.5 MB (50.00%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.c
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.c
@@ -2,6 +2,11 @@ int compare(const void* a, const void* b) {
     return *(int*)a - *(int*)b;
 }
 int minOperations(int** grid, int gridSize, int* gridColSize, int x) {
+    for(int i = 0; i < gridSize; i++) {
+        for(int j = 0; j < gridColSize[0]; j++) {
+            if((grid[i][j] - grid[0][0]) % x != 0) return -1;
+        }
+    }
     int r = gridSize, c = gridColSize[0], k = 0, ans = 0; 
     int* hash = (int*)malloc((r * c) * sizeof(int));
     for(int i = 0; i < r; i++) {
@@ -10,9 +15,6 @@ int minOperations(int** grid, int gridSize, int* gridColSize, int x) {
         }
     }    
     qsort(hash, k, sizeof(int), compare);
-    for(int i = 0; i < k; i++) {
-        if((hash[i] - hash[0]) % x != 0) return -1;
-    }
     int median = hash[k / 2];
     for(int i = 0; i < k; i++) {
         ans += abs(hash[i] - median) / x;
