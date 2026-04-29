ea770cc35650887458e12674e0176ed48d580187
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-29 14:00:55 +0530

    Time: 4 ms (50.00%) | Memory: 13.9 MB (20.00%) - LeetSync

--- 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.c
+++ 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.c
@@ -1,10 +1,10 @@
 int longestAlternatingSubarray(int* nums, int numsSize, int threshold) {
     int ans = 0;
     for(int i = 0; i < numsSize; i++) {
-        if((nums[i] & 1) == 0) {
+        if(nums[i] % 2 == 0) {
             int f = 0, c = 0;
             for(int j = i; j < numsSize; j++, f ^= 1) {
-                if(nums[j] <= threshold && (nums[j] & 1) == f) c++;
+                if(nums[j] <= threshold && nums[j] % 2 == f) c++;
                 else break;
             }
             ans = fmax(ans, c);
