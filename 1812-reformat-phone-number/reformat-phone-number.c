f334fc5aba294cc01e2025c17c540081e56bffc6
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-04-30 09:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (22.22%) - LeetSync

--- 1812-reformat-phone-number/reformat-phone-number.c
+++ 1812-reformat-phone-number/reformat-phone-number.c
@@ -2,10 +2,11 @@ char* reformatNumber(char* number) {
     char* ans = (char*)malloc(150 * sizeof(char));
     int i = 0, j = 0, c = 0;
     while(number[i]) {
-        if(c == 3) {
-            ans[j++] = '-';
-            c = 0;
-        } if(number[i] >= '0' && number[i] <= '9') {
+        if(number[i] >= '0' && number[i] <= '9') {
+            if(c == 3) {
+                ans[j++] = '-';
+                c = 0;
+            }
             ans[j++] = number[i];
             c++;
         } i++;
@@ -14,7 +15,6 @@ char* reformatNumber(char* number) {
         ans[j - 2] = ans[j - 3];
         ans[j - 3] = '-';
     }
-    if(ans[j - 1] == '-') ans[j - 1] = '\0';
     ans[j] = '\0';
     return ans;
 }
