c81067d29005149160d9a221b6ab390937a974fd
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-05-16 23:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (9.33%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.c
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.c
@@ -2,7 +2,7 @@ int findMin(int* nums, int numsSize) {
     int l = 0, r = numsSize - 1;
     if(nums[l] < nums[r]) return nums[l];
     while(l < r) {
-        int mid = l + (r - l) / 2;
+        int mid = (l + r) / 2;
         if(nums[mid] > nums[r]) l = mid + 1;
         else if(nums[mid] < nums[r]) r = mid;
         else r--;
