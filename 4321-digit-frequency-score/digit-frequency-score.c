865e1e2ac218229011641e0cbee455b59f097009
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-05-31 13:48:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (100.00%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.c
+++ 4321-digit-frequency-score/digit-frequency-score.c
@@ -1,10 +1,8 @@
 int digitFrequencyScore(int n) {
-    int* hash = (int*)calloc(10, sizeof(int));
     int ans = 0, x = n;
     while(x > 0) {
-        hash[x % 10]++;
+        ans += x % 10;
         x /= 10;
     }
-    for(int i = 0; i < 10; i++) ans += (i * hash[i]);
     return ans; 
 }
