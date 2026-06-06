d49a8d2325374304db463fdf82c9e18a6ba4481e
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-06-07 00:00:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (96.49%) - LeetSync

--- 4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.c
+++ 4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.c
@@ -1,8 +1,5 @@
 bool consecutiveSetBits(int n) {
-    int c = 0, x = n;
-    while(x > 2) {
-        if((x & 3) == 3) c++;
-        x >>= 1;
-    }
-    return c == 1;
+    int x = n & (n>>1);
+    if(x == 0) return false;
+    return (x & (x-1)) == 0;
 }
