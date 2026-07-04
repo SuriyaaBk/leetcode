3a4e427922293c9a202b6fe8f96c819106693f6d
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-07-04 22:22:19 +0530

    Time: 8 ms (6.54%) | Memory: 22.7 MB (68.22%) - LeetSync

--- 4341-maximum-valid-pair-sum/maximum-valid-pair-sum.c
+++ 4341-maximum-valid-pair-sum/maximum-valid-pair-sum.c
@@ -1,7 +1,8 @@
 int maxValidPairSum(int* nums, int numsSize, int k) {
     int ans = 0;
-    for(int i = 1; i < numsSize; i++) {
-        if(i >= k) ans = fmax(ans, nums[i] + nums[i - k]);
+    for(int i = 1; i < k; i++) nums[i]  =fmax(nums[i], nums[i - 1]);
+    for(int i = k; i < numsSize; i++) {
+        ans = fmax(ans, nums[i] + nums[i - k]);
         nums[i] = fmax(nums[i], nums[i - 1]);
     }
     return ans;
