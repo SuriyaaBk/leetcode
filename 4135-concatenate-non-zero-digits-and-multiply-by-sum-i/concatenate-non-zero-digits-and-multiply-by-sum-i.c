a9ddae95dca9f17fefce39e9f57d780fd158cf98
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-07-07 23:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (63.08%) - LeetSync

---
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.c
@@ -0,0 +1,12 @@
+long long sumAndMultiply(int n) {
+    int x = 0, sum = 0, i = 1;
+    while(n > 0) {
+        int m = n % 10;
+        n /= 10;
+        if(m == 0) continue;
+        sum += m;
+        x += (m * i);
+        i *= 10;
+    }
+    return (long long) x * sum;
+}
