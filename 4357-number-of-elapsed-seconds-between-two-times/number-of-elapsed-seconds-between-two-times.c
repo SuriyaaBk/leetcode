b87be01c57f73c6bed997c2348360db2222d11d6
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-07-12 13:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (86.48%) - LeetSync

--- 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.c
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.c
@@ -1,7 +1,7 @@
-int seconds(char* a) {
-    int h = (a[0] - '0') * 10 + (a[1] - '0');
-    int m = (a[3] - '0') * 10 + (a[4] - '0');
-    int s = (a[6] - '0') * 10 + (a[7] - '0');
+int seconds(char* time) {
+    int h = (time[0] - '0') * 10 + (time[1] - '0');
+    int m = (time[3] - '0') * 10 + (time[4] - '0');
+    int s = (time[6] - '0') * 10 + (time[7] - '0');
     return h * 3600 + m * 60 + s;
 }
 int secondsBetweenTimes(char* startTime, char* endTime) {
