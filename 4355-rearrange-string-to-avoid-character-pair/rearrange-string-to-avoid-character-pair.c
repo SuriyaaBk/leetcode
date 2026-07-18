1a6fda5b390cbe12923714b8bfab5a7de9aa42b8
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-07-18 22:41:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (68.30%) - LeetSync

--- 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.c
+++ 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.c
@@ -2,13 +2,10 @@ char* rearrangeString(char* s, char x, char y) {
     int j = 0, k = 0;
     char* ans = (char*)malloc(101 * sizeof(char));
     for(int i = 0; s[i]; i++) {
-        if(s[i] == x) k++;
-        else ans[j++] = s[i];
+        if(s[i] != x) ans[j++] = s[i];
+        else k++;
     }
-    while(k > 0) {
-        ans[j++] = x;
-        k--;
-    }
+    while(k-- > 0) ans[j++] = x;
     ans[j] = '\0';
     return ans;
 }
