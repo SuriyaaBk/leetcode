65cea773f4d10d780641a8e936c24575560c4575
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-08-31 00:38:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (31.25%) - LeetSync

---
+++ 4410-count-integers-appearing-in-a-single-block/count-integers-appearing-in-a-single-block.c
@@ -0,0 +1,15 @@
+int countSpecialIntegers(int* nums, int numsSize) {
+    int ans = 0;
+    int* hash = (int*)calloc(101, sizeof(int));
+    hash[nums[0]] = 1;
+    for(int i = 1; i < numsSize; i++) {
+        if(nums[i] != nums[i - 1]) {
+            if(hash[nums[i]] == 0) hash[nums[i]] = 1;
+            else hash[nums[i]] = -1;
+        }
+    }
+    for(int i = 0; i <= 100; i++) {
+        if(hash[i] == 1) ans += 1;
+    }
+    return ans;
+}
