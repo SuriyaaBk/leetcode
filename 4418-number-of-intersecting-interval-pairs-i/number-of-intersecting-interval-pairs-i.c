5d43d2b623bb451c2bd6ca90093c5b774081616a
Suriyaa B K <155650038+SuriyaaBk@users.noreply.github.com>
2026-09-20 16:55:19 +0530

    Time: 4 ms (65.12%) | Memory: 12.3 MB (43.72%) - LeetSync

--- 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.c
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.c
@@ -2,7 +2,7 @@ int countIntersectingIntervals(int** intervals, int intervalsSize, int* interval
     int ans = 0;
     for(int i = 0; i < intervalsSize - 1; i++) {
         for(int j = i + 1; j < intervalsSize; j++) {
-            if(intervals[i][0] <= intervals[j][1] && intervals[j][0] <= intervals[i][1]) ans++;
+            if(intervals[j][0] <= intervals[i][1] && intervals[i][0] <= intervals[j][1]) ans++;
         }
     }
     return ans; 
